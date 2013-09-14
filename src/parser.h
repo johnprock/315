633c60706d3d91c51fce53fa7f6052567d43b6fc
Patrick Rock <prock2011@gmail.com>
2013-09-14 16:00:21 -0500

    made declarations for parser class

--- src/parser.h
+++ src/parser.h
@@ -7,5 +7,68 @@
 using namespace std;
 
 class parser {
-  bool parse();
+  public:
+  	// main parsing function
+    bool parse();
+  
+  private:
+  	// parsing helper functions, called by parse()
+  	// query parsing functions
+  	bool parse_query();
+
+    bool parse_relation();
+
+    bool parse_identifier();
+
+    bool parse_expr();
+
+    bool parse_atomic_expr();
+
+    bool parse_selection();
+
+    bool parse_condition();
+
+    bool parse_conjuction();
+
+    bool parse_comparison();
+
+    bool parse_operand();
+
+    bool parse_attribute_name();
+
+    bool parse_projection();
+
+    bool parse_attribute_list();
+
+    bool parse_renaming();
+
+    bool parse_union();
+
+    bool parse_difference();
+
+    bool parse_product();
+
+    //command parsing functions
+    bool parse_command();
+
+    bool parse_open();
+
+    bool parse_close();
+
+    bool parse_write();
+
+    bool parse_exit();
+
+    bool parse_show();
+
+    bool parse_create();
+
+    bool parse_update();
+
+    bool parse_insert();
+
+    bool parse_delete();
+
+    bool parse_typed_attribute_list();
+
 };
