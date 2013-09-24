bffff9e17f4657ea81eadd984a33d08c346027f0
Patrick Rock <prock2011@gmail.com>
2013-09-24 16:44:04 -0500

    fleshed out create

--- src/simpleparse.cpp
+++ src/simpleparse.cpp
@@ -13,10 +13,25 @@ class SimpleParser {
 
     }
 
+  // This simple parser integrates with the dbEngine and provides necessary core functions for building our application
 	bool parse(string input) {
 		    // create
         if(input[0] == 'C') {
-          // call dbengine
+          // process Create Table
+
+          // First we extract the table name
+          int start = 13;
+          int end = ( input.find("(") - 2 ); // name ends two spaces before first open paren
+          string name;
+
+          // Then we get the Attributes, for now assume one attribute
+          Attibute attr;
+
+
+
+          // Then we get the Primary Keys
+
+          // Finally we make the dbengine call
         }
 
         // insert
