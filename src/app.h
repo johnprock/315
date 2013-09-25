cb0eb8f27318ceaba54337721f71a3055f8002a7
Patrick Rock <prock2011@gmail.com>
2013-09-25 18:11:04 -0500

    added application file

---
+++ src/app.h
@@ -0,0 +1,12 @@
+// Patrick Rock
+// 9/25/2013
+
+// This class represents the final application
+class App {
+
+  // constructor
+  App();
+
+  // initialize the application and start user dialogue
+  init();
+}
